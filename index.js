6ad74d702271df07bfd8d5afb5cb5d4749677361
tushari789 <tushar789imran@gmail.com>
2023-06-14 12:59:49 +0600

    category and news part is done

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors  = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const category = require('./data/categories.json');
+const category = require("./data/categories.json");
+const news = require("./data/news.json");
 
-app.get('/', (req, res)=>{
-    res.send('News API running')
+app.get("/", (req, res) => {
+  res.send("News API running");
+});
+
+app.get("/news-categories", (req, res) => {
+  res.send(category);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews)
+});
+
+app.get('/news', (req, res)=>{
+    res.send(news)
 })
 
-app.get('/news-categories', (req, res)=>{
-    res.send(category)
-})
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
 
-app.listen(port, ()=>{
-    console.log("Dragon News Server on port", port);
-})
+  if (id === "8") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
 
-app.listenerCount(port, ()=>{
-    console.log("fire news server running port", port);
-}) 
+app.listen(port, () => {
+  console.log("Dragon News Server on port", port);
+});
+
+app.listenerCount(port, () => {
+  console.log("fire news server running port", port);
+});
